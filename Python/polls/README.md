0834d876b098c3dc34e89dbacddce9deb6b3832d
David Paul Graham <dpgraham4401@gmail.com>
2023-01-29 10:48:09 -0500

    aiohttp polls demo, a simmple http server with asyncio

---
+++ Python/polls/README.md
@@ -0,0 +1,8 @@
+# Polls
+
+This is a demo of the [aiohttp](https://docs.aiohttp.org/en/stable/) python package, as
+documented in their demos page
+here https://aiohttp-demos.readthedocs.io/en/latest/tutorial.html#aiohttp-demos-polls-getting-started
+
+only difference is i'm not going to set up a db for a demo app.
+I want trying to check out aiohttp, not dba.
---
+++ Python/polls/aiohttpdemo_polls/views.py
@@ -0,0 +1,13 @@
+# aiohttpdemo_polls/views.py
+import aiohttp.web_request
+import aiohttp_jinja2
+
+questions_data = [{'title': 'foo', 'text': 'this is a question?'},
+                  {'title': 'bar', 'text': 'this is a jazzy fizzle?'}]
+
+
+@aiohttp_jinja2.template('index.html')
+async def index(request: aiohttp.web_request.Request):
+    print(request.url)
+    questions = questions_data
+    return {"questions": questions}
