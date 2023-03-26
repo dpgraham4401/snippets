3e90f31dccfbd9a5d60e138d4cdb8e86605d3c93
David Paul Graham <dpgraham4401@gmail.com>
2023-03-26 08:22:20 -0400

    more java intro

--- Java/hello-world/src/main/java/org/example/Main.java
+++ Java/hello-world/src/main/java/org/example/Main.java
@@ -1,34 +1,19 @@
 package org.example;
 // Java packages often follow a reverse domain
 
+// we can import classes following a dot style notation
+
+import org.foo.Foo;
+
 // The 'Main' class (note capitalization) has special significance
 public class Main {
     // The main method of the Main class is also special
     public static void main(String[] args) {
-        // we can call this method without instantiating b/c it's a static method
-        FooBar.hello();
-        // To instantiate a new object: <ClassName> <VarName> = new <ClassName>()
-        FooBar foo = new FooBar(1);
-        foo.booBoo();
-    }
-}
-
-// I can declare another class in the same file, but another public class should be in another file
-class FooBar {
-    int boo;
-
-    public FooBar(int boo) {
-        this.boo = boo;
-    }
-
-    // methods need to be explicitly declared as 'public', or they're private
-    // 'static' indicates that method belong to the class, not the instance
-    // 'void' indicates the return type
-    public static void hello() {
-        System.out.println("Hello world!");
-    }
-
-    public void booBoo() {
-        System.out.printf("boo is %d", this.boo);
+        // 'static' class methods can be called without instantiating
+        Foo.sayFoo(true);
+        // To instantiate a new object: <ClassName> <VarName> = new <ClassName>(<args>)
+        Foo foo = new Foo(1, 100);
+        foo.incrementBar(); // good practice camel case for methods/attributes/variables. pascal case for classes
+        foo.incrementBar();
     }
 }
