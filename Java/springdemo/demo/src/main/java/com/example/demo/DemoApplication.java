471e53b881db73e18beb38554c115a634f3c0fbe
David Paul Graham <dpgraham4401@gmail.com>
2023-11-05 08:34:22 -0500

    add SpringBootApplication class RestController to show creating a handler

--- Java/springdemo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Java/springdemo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,15 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // This project was bootstrapped with sprint boot initializer
 
-@SpringBootApplication
+@SpringBootApplication // This decorator tells spring boot this class is the starting point of the application
+@RestController // The rest controller decorator creates a http handler
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public static String hello(@RequestParam(value="name", defaultValue="World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
