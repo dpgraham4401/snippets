4815a793ddb25e37300c481d8ff1384dd2424c20
David Paul Graham <dpgraham4401@gmail.com>
2024-03-18 09:23:36 -0400

    streamline java hello world example

---
+++ Java/hello-world/src/main/java/org/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java.org.helloworld;
+// Java packages follow a reverse domain. The package name should match the directory structure
+
+
+// The 'Main' class with the same name as the file has special significance
+public class HelloWorld {
+    // The main method of the Main class is also special
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
