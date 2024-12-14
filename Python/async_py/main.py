7c9039e36bd58bedaf384238f48e85515a9d2156
David Paul Graham <dpgraham4401@gmail.com>
2024-12-14 11:31:29 -0500

    add timed decorator

--- Python/async_py/main.py
+++ Python/async_py/main.py
@@ -2,25 +2,21 @@ import asyncio
 import time
 
 from async_py.client import Client
+from async_py.utils import timed
 
+
+@timed
 async def run_placeholder_import() -> None:
     """Get all ToDos from json placeholder.
 
     This currently does not operate as expected. The total run time is still a
     cumulative of the long-running calls, instead of just their longest call.
     """
-    start = time.time()
     client = Client()
     todos = await client.get_todos()
-    end_todos = time.time()
     todo_1 = await client.get_todo(1)
-    end_todo = time.time()
     print(todos)
     print(todo_1)
-    end = time.time()
-    print(f"Time to get all ToDos: {end_todos - start:.2f} seconds")
-    print(f"Time to get a single ToDo: {end_todo - start:.2f} seconds")
-    print(f"Time: {end - start:.2f} seconds")
 
 
 def main() -> None:
---
+++ Python/async_py/utils.py
@@ -0,0 +1,43 @@
+"""Utility functions for async_py.
+
+See timed decorator https://gist.github.com/crizCraig/507e98a6ec6cecd0ed4db7170d540e2b.
+"""
+
+import asyncio
+import time
+from typing import Callable, Any
+
+def get_duration_str(start: float) -> str:
+    """Get human readable duration string from start time"""
+    duration = time.time() - start
+    if duration > 1:
+        duration_str = f'{duration:,.3f}s'
+    elif duration > 1e-3:
+        duration_str = f'{round(duration * 1e3)}ms'
+    elif duration > 1e-6:
+        duration_str = f'{round(duration * 1e6)}us'
+    else:
+        duration_str = f'{duration * 1e9}ns'
+    return duration_str
+
+def timed(fn: Callable[..., Any]) -> Callable[..., Any]:
+    def wrapped_fn(*args: Any, **kwargs: Any) -> Any:
+        start = time.time()
+        print(f'Running {fn.__name__}...')
+        ret = fn(*args, **kwargs)
+        duration_str = get_duration_str(start)
+        print(f'Finished {fn.__name__} in {duration_str}')
+        return ret
+
+    async def wrapped_fn_async(*args: Any, **kwargs: Any) -> Any:
+        start = time.time()
+        print(f'Running {fn.__name__}...')
+        ret = await fn(*args, **kwargs)
+        duration_str = get_duration_str(start)
+        print(f'Finished {fn.__name__} in {duration_str}')
+        return ret
+
+    if asyncio.iscoroutinefunction(fn):
+        return wrapped_fn_async
+    else:
+        return wrapped_fn
