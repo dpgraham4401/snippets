16c71ce6290fde7c440f52cd05d44a6a02dff8e0
David Paul Graham <dpgraham4401@gmail.com>
2025-04-24 21:51:27 -0400

    refactor to use a if main block to call the example function

--- Python/scientific/csv_files.py
+++ Python/scientific/csv_files.py
@@ -8,13 +8,17 @@ using the std lib csv files or pandas.
 
 import csv
 
-with open("./mini_no_headers.csv") as csv_file:
-    csv_reader = csv.DictReader(csv_file, fieldnames=("foo", "bar", "baz", "fee", "fii"))
-    tie_count: dict[str, int] = {}
-    for row in csv_reader:
-        tie_name = row["baz"]
-        if tie_name in tie_count:
-            tie_count[tie_name] += 1
-        else:
-            tie_count[tie_name] = 1
-    print(tie_count)
+def read_mini_without_headers() -> None:
+    with open("./mini_no_headers.csv") as csv_file:
+        csv_reader = csv.DictReader(csv_file, fieldnames=("foo", "bar", "baz", "fee", "fii"))
+        tie_count: dict[str, int] = {}
+        for row in csv_reader:
+            tie_name = row["baz"]
+            if tie_name in tie_count:
+                tie_count[tie_name] += 1
+            else:
+                tie_count[tie_name] = 1
+
+
+if __name__ == "__main__":
+    read_mini_without_headers()
