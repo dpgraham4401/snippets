eb016a8a303699237cee74af27c1da48a6e935c9
David Graham <dpgraham4401@gmail.com>
2025-04-27 10:59:20 -0400

    filling empty with the average We're using a functional programming paradigm to keep the program side-effect free, possibly at the expense of performance

--- Python/scientific/sensor_analysis.py
+++ Python/scientific/sensor_analysis.py
@@ -2,10 +2,13 @@
 
 import logging
 
+import numpy as np
 import pandas as pd
 from pandas import DataFrame
 
 logger = logging.getLogger(__name__)
+
+pd.options.mode.copy_on_write = "warn"
 
 
 class SensorProcessError(Exception):
@@ -23,6 +26,28 @@ def read_csv_with_missing_data() -> DataFrame:
         raise SensorProcessError(msg) from exc
 
 
-if __name__ == "__main__":
+def fill_empty_with_average(data: DataFrame, name: str) -> DataFrame:
+    """Create a dataframe with empty/NaN values filled with the column average.
+
+    Note:
+        starting in pandas > 3.0, modifying a copy or another reference of a dataframe
+        will use Copy on Write (CoW) to avoid modifying the original dataframe.
+        As long as we don't use .copy(deep=False), the original dataframe will be
+        left unchanged. With 3.0, we just won't need to use the copy method, just assign.
+    """
+    data_copy = data.copy()
+    avg = data_copy[name].mean()
+    data_copy.fillna({name: avg}, inplace=True)
+    return data_copy
+
+
+def main():
+    """Application entry point."""
     data = read_csv_with_missing_data()
+    clean_data = fill_empty_with_average(data=data, name="temperature_c")
+    print(clean_data.head())
     print(data.head())
+
+
+if __name__ == "__main__":
+    main()
