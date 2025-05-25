16d757e627481954644e2c83c47db04225e45d6d
David Graham <dpgraham4401@gmail.com>
2025-05-25 19:23:16 -0400

    basic usage of callbacks to for async operations like reading a file

--- node/express/app.js
+++ node/express/app.js
@@ -1,18 +1,17 @@
-import path from "path";
-import { fileURLToPath } from "url";
+import { readFile, writeFile } from "fs";
 
-console.log(path.sep); // '/'
-
-const filePath = path.join("/content/", "subfolder", "test.txt");
-console.log(filePath); // '/content/subfolder/test.txt'
-
-const base = path.basename(filePath);
-console.log(base); // 'test.txt'
-
-// note, we need to import dirname instead of relying on __dirname global when using ES modules
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
-
-// We could also just remove __dirname here and it would be the same result
-const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt");
-console.log(absolute);
+readFile("./content/first.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  const processedData = `Wll hello there, ${result}`;
+  writeFile("./content/results.txt", processedData, (err, data) => {
+    if (err) {
+      console.log("write error", err);
+      return;
+    }
+    console.log("Done writing to file");
+  });
+});
