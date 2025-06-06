20bc3cfbc7c8fc019f9a36d7f4e0ddc47719e628
David Paul Graham <dpgraham4401@gmail.com>
2025-06-06 14:35:52 -0400

    using threads to speed up blocking IO bound work

--- Python/async_python/async_py/py_threads.py
+++ Python/async_python/async_py/py_threads.py
@@ -4,10 +4,15 @@ Even though Python has the GIL, multithreading is a great way to handle IO bound
 for example using legacy popular HTTP libraries like requests.
 Blocking IO releases the global interpreter lock, enabling the possibility of running IO
 concurrently.
+
+concurrent.futures pkg provides ThreadPoolExecutor, which will create and maintain a pool
+of thread that we can submit work to.
 """
 
+from concurrent.futures import ThreadPoolExecutor
 from threading import Thread
 import socket
+import time
 
 import requests
 
@@ -18,6 +23,15 @@ def get_status_code(url: str) -> int:
 
 
 if __name__ == "__main__":
-    url = "https://www.example.com"
-    print(get_status_code(url))
-    print(get_status_code(url))
+    urls = ["https://www.example.com" for _ in range(1000)]
+    start = time.time()
+
+    ### Synchronous --> about 35 seconds (Dell Latitude)
+    # results = [get_status_code(u) for u in urls]
+
+    ### Threaded --> about 5 seconds (same Dell Latitude)
+    with ThreadPoolExecutor() as pool:
+        results = pool.map(get_status_code, urls)
+
+    end = time.time()
+    print(f"finished request in {end - start:.4f} seconds")
