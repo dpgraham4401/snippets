3adb57d1876b77dd23e3d51082e55afab794428c
David Paul Graham <dpgraham4401@gmail.com>
2025-06-06 14:27:15 -0400

    example making a couple requests with the requests library

--- Python/async_python/async_py/py_threads.py
+++ Python/async_python/async_py/py_threads.py
@@ -1,26 +1,23 @@
 """Using Threads in Python to handle blocking work.
 
-Even though Python has the GIL, multithreading is a great way to handle IO bound synchronous code, 
+Even though Python has the GIL, multithreading is a great way to handle IO bound synchronous code,
 for example using legacy popular HTTP libraries like requests.
-Blocking IO releases the global interpreter lock, enabling the possibility of running IO 
+Blocking IO releases the global interpreter lock, enabling the possibility of running IO
 concurrently.
 """
 
 from threading import Thread
 import socket
 
-def echo(client: socket.socket):
-    while True:
-        data = client.recv(2048)
-        print(f"received {data!r}, sending!")
-        client.sendall(data)
+import requests
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(('127.0.0.1', 8000))
-    server.listen()
-    while True:
-        connection, _ = server.accept()
-        thread = Thread(target=echo, args=(connection,))
-        thread.start()
+def get_status_code(url: str) -> int:
+    response = requests.get(url)
+    return response.status_code
+
+
+if __name__ == "__main__":
+    url = "https://www.example.com"
+    print(get_status_code(url))
+    print(get_status_code(url))
