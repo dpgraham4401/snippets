507284e905bd25097360f03e58dd18f967df9064
David Paul Graham <dpgraham4401@gmail.com>
2025-07-01 12:25:21 -0400

    difference between function expressions and function declarations

--- javascriptNinja/chpThree.js
+++ javascriptNinja/chpThree.js
@@ -33,3 +33,9 @@ async function selfMemoizingFn(x) {
 selfMemoizingFn(5).then(console.log); // 10, takes 1 second
 selfMemoizingFn(5).then(console.log); // 10, but this time it's instant
 
+// A 'function expression' is just when a function is used as part of another statement
+const myFunction = function (x) {  // notice the lack of a name after 'function'
+    return x * 2;
+}
+// The alternative (common) is just called a 'function declaration'
+function aDeclaredFunction() {}
