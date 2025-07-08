fcedb6991b877b87ba89f97f2c69db917e5e05d0
David Graham <dpgraham4401@gmail.com>
2025-07-07 20:33:31 -0400

    add explanation about how to use type hints with cython

--- Python/hello-cython/hello.pyx
+++ Python/hello-cython/hello.pyx
@@ -1,12 +1,19 @@
+"""Using Cython.
+
+Cython is a superset of Python (all valid Python is valid cython) that compiles to C extensions modules.
+We gain speed by adding Cython package type hints to our pyx Python code, using C files, and generally finding
+ways to release the GIL (Global Interpreter Lock) in Python.
+
+There are two ways to use Cython in a .pyx file:
+1. Pure Python type hints (PEP 484 style) from the `cython` module
+2. Cython, using C type hints (using the `cdef` keyword)
+"""
 import cython
 
-def say_hello(name: str) -> str:
-    return f"Hello {name}"
-
-@cython.boundscheck(False)
-def cy_sum_squares(int n):
-    cdef long long total = 0
-    cdef int i
+def cy_sum_squares(int n) -> cython.long:
+    """We're using PEP hints, the comments show the Cython equivalent."""
+    total: cython.long = 0  # cdef long total = 0
+    i: cython.int  # cdef int i
     for i in range(1, n + 1):
         total += i * i
     return total
