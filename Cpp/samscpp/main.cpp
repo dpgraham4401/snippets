7541d4dba1057b254689528d7b090e6198b310bb
David Graham <dpgraham4401@gmail.com>
2025-07-07 20:31:06 -0400

    Chapter two

--- Cpp/samscpp/main.cpp
+++ Cpp/samscpp/main.cpp
@@ -1,8 +1,27 @@
-#include <iostream>
+/**
+ * Parts of a program
+*/
 
+// Preprocessor directives
+// All start with a hash (#), they run before the compiler.
+#include <iostream> // often called 'hash include', 'sharp include', or 'pound include'
+// we use quotes ("") for local files and angle brackets (<>) for standard library files
+// A large number of includes can slow down compilation, however with C++20 modules, this is less of an issue.
+
+// By convention, main returns an int, and 0 for success, -1 for error, used by OS.
 int main() {
-    for (int i = 0; i < 5; ++i) {
-        std::cout << "i = " << i << std::endl;
+    // Can declare that we're using a namespace in a function or globally in a file.
+    using namespace std;
+    int nLoops;
+    cout << "Enter number of loops: ";
+    cin >> nLoops;
+    if (cin.fail()) {
+        cout << "Invalid input." << endl;
+        return EXIT_FAILURE; // or -1, same-same
     }
-    return EXIT_SUCCESS;
+    for (int i = 0; i < nLoops; ++i) {
+        // "standard see-out and "standard see-in"
+        cout << "i = " << i << endl;
+    }
+    return EXIT_SUCCESS; // or 0, same-same
 }
