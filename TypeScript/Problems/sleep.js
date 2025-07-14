3b2d7981dd974bc663cbaaffb49c4df52aa4752a
David Graham <dpgraham4401@gmail.com>
2025-07-14 12:28:16 -0400

    Example sleep function with an explaination of the Promise constructor's expected args

---
+++ TypeScript/Problems/sleep.js
@@ -0,0 +1,28 @@
+/**
+ * Example Problem: create a sleep function that takes a number of milliseconds.
+ */
+
+async function sleep(ms) {
+    // The trick here is simply create a new promise.
+    // Promise constructor an 'executor', which is a function that accepts 2 callbacks
+    // resolveFn, and rejectFn. Resolve is called (with the expected return value) to indicate success
+    // rejectFn is called (with the error) to indicate failure.
+    // Note, we don't define resolveFn, rejectFn.
+    return new Promise( (resolve, reject) => {
+        try {
+            setTimeout(resolve, ms)
+        } catch (error) {
+            console.log(error.message)
+            reject(error)
+        }
+    })
+}
+
+
+async function main() {
+    console.log("hello")
+    await sleep(1000)
+    console.log("bye")
+}
+
+main();
