302d63e100dcd3c62ac09b0dc30a6e909643cdf8
David Paul Graham <dpgraham4401@gmail.com>
2025-07-17 21:45:01 -0400

    Set up a vite vanilla typescript project with prettier, daisy UI and tailwindcss, eslint

---
+++ javascriptNinja/ninjadom/eslint.config.js
@@ -0,0 +1,11 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-config-prettier';
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended,
+];
