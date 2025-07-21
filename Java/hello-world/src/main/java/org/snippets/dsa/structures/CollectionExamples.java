f8377b59af747fb868abd62e90be2b33a6bb02ee
David Paul Graham <dpgraham4401@gmail.com>
2025-07-21 15:07:31 -0400

    using the List interface

--- Java/hello-world/src/main/java/org/snippets/dsa/structures/CollectionExamples.java
+++ Java/hello-world/src/main/java/org/snippets/dsa/structures/CollectionExamples.java
@@ -1,6 +1,7 @@
 package org.snippets.dsa.structures;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * As we talked about in {@code  ./IterableInterfaceExamples}, the Collection interface extends the Iterable interface.
@@ -13,5 +14,15 @@ public class CollectionExamples {
         java.util.Collections.addAll(myCollection, "a", "b", "c"); // instead of using .add("a") multiple times.
         System.out.println(myCollection);
         System.out.println(myCollection.isEmpty());
+        usingLists();
+    }
+
+    public static void usingLists() {
+        List<String> list = new ArrayList<String>();
+        java.util.Collections.addAll(list, "foo", "bar", "quq", "bar", "baz");
+        System.out.println(list.indexOf("foo"));
+        System.out.println(list.indexOf("asdf"));
+        System.out.println(list.lastIndexOf("bar"));
+        System.out.println(list.subList(0, 2)); // 0 up to (but not including) index 2
     }
 }
