6d17ec857b5f6b7eee67e7c119dcb998b06fdbe1
David Paul Graham <dpgraham4401@gmail.com>
2025-07-21 15:07:36 -0400

    rename IterableExample class for concise naming

--- Java/hello-world/src/main/java/org/snippets/dsa/structures/IterableExamples.java
+++ Java/hello-world/src/main/java/org/snippets/dsa/structures/IterableExamples.java
@@ -11,7 +11,7 @@ import java.util.Iterator;
  * There are various concrete classes in the std lib of these three interfaces, such as
  * ArrayList, LinkedList, PriorityQueue, and HasSet.
  */
-public class IterableInterfaceExample {
+public class IterableExamples {
 
     public static void main(String[] args) {
         String[] myList = {"foo", "bar", "baz"};
