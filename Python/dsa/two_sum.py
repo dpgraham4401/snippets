6bbf0aed54e8e7de389caeb9708e5ee9491b082c
David Graham <dpgraham4401@gmail.com>
2025-08-15 11:20:45 -0400

    practice example problem using a hash map to build a map over an iterator

--- Python/dsa/two_sum.py
+++ Python/dsa/two_sum.py
@@ -3,16 +3,18 @@ Given a list of integers, return the indices of two numbers that add to the targ
 The array is not guaranteed to be sorted, every solution will have exactly one solution.
 """
 
+
 def two_sums(nums: list[int], target) -> tuple[int, int]:
-    map: dict = {}
-    for i, val in enumerate(nums):
-        diff = target - val
-        if diff in map:
-            return (map[diff], i)
-        map[val] = i
-    raise ValueError("No two sum solution found")
+    diffs: dict[int, int] = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in diffs:
+            return diffs[diff], i
+        diffs[num] = i
+    err_msg = "No solution found"
+    raise ValueError(err_msg)
 
 
-nums = [3,4,5,6]
-target = 7
+nums = [3, 4, 5, 6]
+target = 9
 print(two_sums(nums, target))
