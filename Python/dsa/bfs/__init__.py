30b0b7e792e91abbb0a700272723f4bd9428a844
David Graham <dpgraham4401@gmail.com>
2025-08-15 12:47:44 -0400

    breadth first search algo pkg with __init__ docstring

---
+++ Python/dsa/bfs/__init__.py
@@ -0,0 +1,6 @@
+"""Breadth First Search
+
+BFS is a graph traversal algorithm that searches a tree/graph for a node that meets
+a given criteria. It starts at a root and explores all adjacent nodes before going deeper
+into the tree (as opposed to Depth First Search, which goes deep then spreads out).
+"""
