a7726e84c2f8188979660eea72e5837860d903e4
David Graham <dpgraham4401@gmail.com>
2025-08-15 11:32:17 -0400

    practice basic problem using a stack again

--- Python/dsa/valid_parantheses.py
+++ Python/dsa/valid_parantheses.py
@@ -7,21 +7,19 @@ last element in the stack, otherwise the paratheses are not balanced.
 
 
 def is_valid(s: str) -> bool:
-    stack = []
+    stack: list[str] = []
     bracket_map = {
         "}": "{",
         "]": "[",
         ")": "(",
-        }
+    }
     for char in s:
         if char in bracket_map:
             if stack and stack[-1] == bracket_map[char]:
                 stack.pop()
-            else:
-                return False
         else:
             stack.append(char)
-    return True
+    return len(stack) == 0
 
 
 brackets = "({{()}[]})"
