6e1a8306884e4af99fcbf919ead112b656489ee0
David Graham <dpgraham4401@gmail.com>
2025-08-15 15:31:38 -0400

    using a deque to implement breadth first search

--- Python/dsa/bfs/__init__.py
+++ Python/dsa/bfs/__init__.py
@@ -3,4 +3,6 @@
 BFS is a graph traversal algorithm that searches a tree/graph for a node that meets
 a given criteria. It starts at a root and explores all adjacent nodes before going deeper
 into the tree (as opposed to Depth First Search, which goes deep then spreads out).
+
+For a great video on the advantages of BFS https://www.youtube.com/watch?v=e69C6xhiSQE
 """
--- Python/dsa/bfs/walls_and_gates.py
+++ Python/dsa/bfs/walls_and_gates.py
@@ -3,7 +3,7 @@
 A common BFS algorithm problem.
 
 You are given an M x N 2D grid initialized with these three possible values:
-    * -1 - A water cell that can not be traversed.
+    * -1 - A water cell that cannot be traversed.
     * 0 - A treasure chest.
     * INF - A land cell that can be traversed.
       We use the integer 2^31 - 1 = 2147483647 to represent INF.
@@ -13,13 +13,70 @@ If a land cell cannot reach a treasure chest, then the value should remain INF.
 Assume the grid can only be traversed up, down, left, or right.
 """
 
-intput = [
+from collections import deque
+
+grid_input = [
     [2147483647, -1, 0, 2147483647],
     [2147483647, 2147483647, 2147483647, -1],
     [2147483647, -1, 2147483647, -1],
     [0, -1, 2147483647, 2147483647],
 ]
 
+expected_output = [[3, -1, 0, 1], [2, 2, 1, -1], [1, -1, 2, -1], [0, -1, 3, 4]]
 
-def map_islands_and_treasure(self, grid: list[list[int]]) -> None:
-    pass
+LAND = 2147483647
+WATER = -1
+CHEST = 0
+
+
+def map_islands_and_treasure(grid: list[list[int]]) -> None:
+    """Map out the number of moves to the nearest treasure chest.
+
+    Instead of finding every piece of land, then for each piece of land find the closest chest,
+    We're going to work backwards from the chests themselves.
+    """
+    ROWS, COLS = len(grid), len(grid[0])
+    visited: set[tuple[int, int]] = set()
+    q: deque[tuple] = deque()
+
+    def visit_square(r: int, c: int) -> None:
+        """Mark a room as visited and add it to the deque if the following criteria is met:
+
+        1. It's not impassible (water)
+        2. It's not out of bounds
+        3. it hasn't already been visited
+        """
+        is_out_of_bounds: bool = r < 0 or r >= ROWS or c < 0 or c >= COLS
+        if is_out_of_bounds:
+            return
+        is_water: bool = grid[r][c] == WATER
+        has_been_visited = (r, c) in visited
+        if is_water or has_been_visited:
+            return
+        visited.add((r, c))
+        q.append((r, c))
+
+    # The first step is to find the treasure chests
+    for r in range(ROWS):
+        for c in range(COLS):
+            if grid[r][c] == CHEST:
+                q.append((r, c))
+                visited.add((r, c))
+
+    dist = 0
+    while q:
+        for _ in range(len(q)):
+            r, c = q.popleft()
+            grid[r][c] = dist
+            visit_square(r + 1, c)
+            visit_square(r - 1, c)
+            visit_square(r, c + 1)
+            visit_square(r, c - 1)
+        dist += 1
+
+
+map_islands_and_treasure(grid_input)
+
+print(grid_input)
+print(expected_output)
+assert grid_input == expected_output
