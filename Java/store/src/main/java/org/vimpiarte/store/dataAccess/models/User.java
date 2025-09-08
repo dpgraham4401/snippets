bf9fa0cc25c732d482a15cc20ab01109a71bd4a9
David Paul Graham <dpgraham4401@gmail.com>
2025-09-08 08:26:20 -0400

    Set up project with lombok, db migrations

---
+++ Java/store/src/main/java/org/vimpiarte/store/dataAccess/models/User.java
@@ -0,0 +1,27 @@
+package org.vimpiarte.store.dataAccess.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
