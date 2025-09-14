416a32d5f9e13448f91c6160c38a34d4d617364f
David Graham <dpgraham4401@gmail.com>
2025-09-14 17:39:16 -0400

    introduce concepts and constraints

--- Cpp/samscpp/lesson14/macros.cpp
+++ Cpp/samscpp/lesson14/macros.cpp
@@ -15,6 +15,9 @@
  *  - They can make things more complex to understand if they're overused.
  *
  *  We should generally avoid using macros when possible.
+ *
+ *  Templates:
+ *  Templates are a more modern and type-safe way to create generic code.
  */
 
 #include <iostream>
@@ -24,6 +27,19 @@
 
 // creating a macro function
 #define SQUARE(x) ((x) * (x))
+
+// Templates are defined using the 'template' keyword
+// template <parameter list>
+
+/**
+ * We can also use a C++20 feature called a "concept"
+ * https://en.cppreference.com/w/cpp/language/constraints.html
+ */
+template<typename Numeric>
+    requires std::is_arithmetic_v<Numeric> // this is a C++20 feature called a "concept"
+Numeric square(const Numeric val) {
+    return val * val;
+}
 
 int main() {
     int twenty_five = SQUARE(5);
